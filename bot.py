1379c425c8c8c16cc8197e674505919800546c56
Siavash <90524474+Syaw0@users.noreply.github.com>
2023-05-20 14:19:48 +0330

    refactor the code!
    The refactored code has been modified to improve readability, maintainability, and efficiency. Here are some of the changes:
    
    1. The code has been split into functions to improve modularity and readability.
    
    2. The `create_data_dir()` function has been added to create a directory to store the file data if it does not exist.
    
    3. The `get_file_data()` function has been added to extract the file ID and type from the message.
    
    4. The `save_file()` function has been added to save the file data to a JSON file in the `data` directory.
    
    5. The `start_handler()` function has been modified to handle the `/start` command and the link to retrieve the file data.
    
    6. The `add_file()` function has been modified to handle photo, video, and document messages.
    
    7. The code has been properly formatted to improve readability.

--- bot.py
+++ bot.py
@@ -4,78 +4,66 @@ import os
 
 bot_token = "BOT TOKEN"
 bot = telebot.TeleBot(bot_token)
-print("Started")
-print("By Yamete_Kudasai_Oni_Chan")
 
+def create_data_dir():
+    try:
+        os.mkdir('data')
+    except:
+        pass
 
-try:
-    os.mkdir('data')
-except:
-    pass
+def get_file_data(message):
+    file_type = message.content_type
+    file_id = None
+    if file_type == 'photo':
+        file_id = message.photo[2].file_id
+    elif file_type == 'document':
+        file_id = message.document.file_id
+    elif file_type == 'video':
+        file_id = message.video.file_id
+    return file_id, file_type
 
+def save_file(message):
+    admins = open("admin_list.txt", "r").read().splitlines()
+    for admin in admins:
+        if str(message.from_user.id) == admin:
+            file_id, file_type = get_file_data(message)
+            if file_id:
+                data_file = open(f"data/{file_id}.json", "w")
+                data = {
+                    'file_type': file_type,
+                    'file_id': file_id
+                }
+                json.dump(data, data_file)
+                bot.send_message(message.chat.id,
+                                 f"success saved\nlink to get file\nt.me/{bot.get_me().username}?start={file_id}",
+                                 reply_to_message_id=message.id)
 
 @bot.message_handler(commands=['start'])
-def StartHandler(message):
+def start_handler(message):
     if message.text == '/start':
         bot.reply_to(message, f"Hello {message.from_user.first_name},\nWelcome to {bot.get_me().first_name}")
-    if len(message.text.split()) == 2:
+    elif len(message.text.split()) == 2:
         file = message.text.split()[1]
-        file_data = open(f"data/{file}.json", "rb")
-        file_data = json.loads(file_data.read())
-        if file_data['file_type'] == 'photo':
-            bot.send_photo(message.chat.id, file_data['file_id'], reply_to_message_id=message.id)
-        if file_data['file_type'] == 'document':
-            bot.send_document(message.chat.id, file_data['file_id'], reply_to_message_id=message.id)
-        if file_data['file_type'] == 'video':
-            bot.send_video(message.chat.id, file_data['file_id'], reply_to_message_id=message.id)
+        try:
+            with open(f"data/{file}.json", "rb") as file_data:
+                file_data = json.loads(file_data.read())
+                file_type = file_data['file_type']
+                file_id = file_data['file_id']
+                if file_type == 'photo':
+                    bot.send_photo(message.chat.id, file_id, reply_to_message_id=message.id)
+                elif file_type == 'document':
+                    bot.send_document(message.chat.id, file_id, reply_to_message_id=message.id)
+                elif file_type == 'video':
+                    bot.send_video(message.chat.id, file_id, reply_to_message_id=message.id)
+        except FileNotFoundError:
+            bot.reply_to(message, "File not found")
 
+@bot.message_handler(content_types=["photo", "video", "document"])
+def add_file(message):
+    create_data_dir()
+    save_file(message)
 
-@bot.message_handler(content_types=["photo"])
-def AddFile(message):
-    admins = open("admin_list.txt", "r").read().splitlines()
-    for admin in admins:
-        if str(message.from_user.id) == admin:
-            data_file = open("data/" + message.photo[2].file_unique_id + ".json", "w")
-            data = {
-                'file_type': f'{message.content_type}',
-                'file_id': message.photo[2].file_id
-            }
-            json.dump(data, data_file)
-            bot.send_message(message.chat.id,
-                             f"success saved\nlink to get file\nt.me/{bot.get_me().username}?start={message.photo[2].file_unique_id}",
-                             reply_to_message_id=message.id)
-
-
-@bot.message_handler(content_types=["video"])
-def AddFile(message):
-    admins = open("admin_list.txt", "r").read().splitlines()
-    for admin in admins:
-        if str(message.from_user.id) == admin:
-            data_file = open("data/" + message.video.file_unique_id + ".json", "w")
-            data = {
-                'file_type': f'{message.content_type}',
-                'file_id': message.video.file_id
-            }
-            json.dump(data, data_file)
-            bot.send_message(message.chat.id,
-                             f"success saved\nlink to get file\nt.me/{bot.get_me().username}?start={message.video.file_unique_id}",
-                             reply_to_message_id=message.id)
-
-
-@bot.message_handler(content_types=["document"])
-def AddFile(message):
-    admins = open("admin_list.txt", "r").read().splitlines()
-    for admin in admins:
-        if str(message.from_user.id) == admin:
-            data_file = open("data/" + message.document.file_unique_id + ".json", "w")
-            data = {
-                'file_type': f'{message.content_type}',
-                'file_id': message.document.file_id
-            }
-            json.dump(data, data_file)
-            bot.send_message(message.chat.id,
-                             f"success saved\nlink to get file\nt.me/{bot.get_me().username}?start={message.document.file_unique_id}",
-                             reply_to_message_id=message.id)
-
-
-bot.infinity_polling()
+if __name__ == '__main__':
+    print("Started")
+    print("By Yamete_Kudasai_Oni_Chan")
+    bot.infinity_polling()
